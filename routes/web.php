4e66c7e358929cba4570665d1375b5d8e9960400
Max46656 <maxfromsea36@gmail.com>
2022-11-25 16:30:47 +0800

    Route web

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::namespace ('App\Http\Controllers')->group(function () {
+
+    Route::get('/', 'SiteController@index');
+    Route::get('shopforshop', 'SiteController@shop');
+
 });
